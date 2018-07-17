cb78bea65e153161389c0657f042b486bd05eb73
Conor Doyle <contact@conordoyle.io>
2018-07-17 09:25:37 +0100

    Fix hero header

--- wp-content/themes/html5blank2/src/p-hero.php
+++ wp-content/themes/html5blank2/src/p-hero.php
@@ -7,7 +7,8 @@
                     <div class="city-image">
                       <div class="container">
                         <div class="col-sm-12 text-center mb-80">
-                            <h4 class="extra-text">Welcome To <span>Lansdowne Executive Search</span></h4>
+                            <h2 class="hero-h2">Welcome To</h2>
+                            <h3 class="hero-h3">Lansdowne Executive Search</h3>
                             <p class="paragraph-text"><span class="bold">Lansdowne Executive Search </span> is a premium level executive search company, specialising in the appointment of senior business leaders and the provision of bespoke consultancy services. We deliver a range of <span class="bold"> Executive Search </span> and <span class="bold">Interim Management </span> solutions, in addition to tailored <span class="bold">Executive Coaching </span> and <span class="bold">Organisational Consulting </span> services. <br><br> Our engagement models follow a carefully crafted approach to ensure the requirements for each assignment are thoroughly understood. Our processes utilise comprehensive, systematic and responsive methodologies. Combining these elements with <span class="bold">our deep market knowledge</span>, we deliver a <span class="bold">highly effective service </span> to client organisations, enabling them to achieve their strategic goals.</p>
                         </div>
                       </div>
