7498cdca5145344969e7599a368d32706b4f6313
Conor Doyle <contact@conordoyle.io>
2018-07-17 12:38:49 +0100

    Scaffold out 'sectors' section

--- wp-content/themes/html5blank2/src/p-sectors.php
+++ wp-content/themes/html5blank2/src/p-sectors.php
@@ -4,46 +4,68 @@
         <!-- =====================================
         ==== Start Sectors Tabs -->
 
-    
 
-        <section class="serv-tabs section-padding bg-img" id="Sectors" data-scroll-index="3">
-            <div class="container">
+
+        <section class="serv-tabs section-padding bg-img" style="padding: 0 0;" id="Sectors" data-scroll-index="3">
+          <div class="doorstep-image-purple">
+            <div class="container" style="width: 60%;">
                 <div class="row">
-                    
+
                     <div class="section-head col-sm-12">
-                        <h4>
-                            <span>Our</span><br>
-                            Sectors
+                        <h4 style="color: #fff; margin-bottom: 1.5em;">
+                            <span style="color: #fff">Our</span><br>
+                            <span style="font-weight: bold;">Sectors</span>
                         </h4>
+                        <p style="color: #fff; text-align: center; margin-bottom: 3em;">Lansdowne Executive Search delivers a range of Executive Search and Interim Management
+solutions, in addition to tailored Executive Coaching and Organisational Consulting services.</p>
+<div style="display: flex; border-left: 1px solid #fff; border-right: 1px solid #fff;">
+  <ul style="color: #fff; font-size: 1.8rem; font-weight: bold; margin-bottom: 0; width: 50%; padding-left: 1em;">
+    <li style="margin-bottom: 1em;">Transport, Aviation & Marine</li>
+    <li style="margin-bottom: 1em;">Construction & Materials</li>
+    <li style="margin-bottom: 1em;">Leisure & Gaming</li>
+    <li style="margin-bottom: 1em;">Energy, Utilities & Natural Resources</li>
+    <li style="margin-bottom: 1em;">Technology</li>
+    <li style="margin-bottom: 1em;">Media & Telecoms</li>
+    <li style="margin-bottom: 1em;">Life Science</li>
+    <li style="margin-bottom: 0">Agri Business</li>
+  </ul>
+  <ul style="color: #fff; font-size: 1.8rem; font-weight: bold; margin-bottom: 0; width: 50%; padding-left: 1em; border-left: 1px solid #fff;">
+    <li style="margin-bottom: 1em;">Food & Drinks</li>
+    <li style="margin-bottom: 1em;">Retail & FMCG</li>
+    <li style="margin-bottom: 1em;">Banking & Financial Services</li>
+    <li style="margin-bottom: 1em;">Professional Sevices& Legal</li>
+    <li style="margin-bottom: 1em;">Finance & Accounting</li>
+    <li style="margin-bottom: 1em;">Public Sector</li>
+    <li style="margin-bottom: 1em;">Not For Profit</li>
+    <li style="margin-bottom: 0;">Education</li>
+  </ul>
+</div>
                     </div>
 
                     <div class="row">
+                      <!-- <?php
+                      $sectors = get_field('sectors', 'options');
+                      if( $sectors ):
+                              $image = $sectors['image'];
+                              $text = $sectors['text'];
+                          ?>
+
+                          <div class="col-md-6">
+                              <img src="<?php echo $image ?> " alt="">
+                          </div>
+                          <div class="col-md-6">
+                              <p><?php echo $text ?> </p>
+                          </div>
+
+                      <?php endif; ?> -->
 
 
-                    <?php
-                    $sectors = get_field('sectors', 'options');	
-                    if( $sectors ): 
-                            $image = $sectors['image'];
-                            $text = $sectors['text'];
-                        ?> 
-
-                        <div class="col-md-6">
-                            <img src="<?php echo $image ?> " alt="">
-                        </div>
-                        <div class="col-md-6">
-                            <p><?php echo $text ?> </p>
-                        </div>
-
-                    <?php endif; ?> 
-
-
-                    </div>
+                      </div>
                     </div>
                 </div>
             </div>
+          </div>
         </section>
 
         <!-- End Sectors Tabs ====
         ======================================= -->
-
-    
